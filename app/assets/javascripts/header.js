43f60bfb77e3de360f7866ada9e32596e2708779
Michael Lin <Terragnome@gmail.com>
2015-10-01 06:41:31 -0700

    Fix hamburger layering.  Hamburger closes on click

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -3,20 +3,28 @@ var Header = Header || {};
 Header.Init = function(url){
 	$(".nav_menu_button").click(Header.ToggleMenu);
 	Header.isMenuOpen = false;
+
+	$(".nav_menu").find("a").click(Header.CloseMenu);
 }
 
 Header.ToggleMenu = function(){
-	if(Header.isMenuOpen){
-		Header.isMenuOpen = false;
-		Header.CloseMenu();
-	}else{
-		Header.isMenuOpen = true;
-		Header.OpenMenu();
-	}
+	Header.isMenuOpen ? Header.CloseMenu() : Header.OpenMenu();
+	Header.isMenuOpen = !Header.isMenuOpen;
 }
 Header.OpenMenu = function(){
-	$(".nav_menu").show();
+	var navMenu = $(".nav_menu");
+	if( !navMenu.hasClass("anim_roll_down") ){
+		navMenu.show();
+		navMenu.addClass("anim_roll_down");
+		navMenu.removeClass("anim_roll_up");
+	}
 }
 Header.CloseMenu = function(){
-	$(".nav_menu").hide();
+	var navMenu = $(".nav_menu");
+	if( !navMenu.hasClass("anim_roll_up") ){
+		navMenu.removeClass("anim_roll_down");
+		navMenu.addClass("anim_roll_up");
+		navMenu.hide();
+		// navMenu.one("transitionend", navMenu.hide);
+	}
 }
