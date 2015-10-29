4b3742618498c0f9194ea07433501ed8f8f42b05
Michael Lin <Terragnome@gmail.com>
2015-10-29 15:57:05 -0700

    Update the seo path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController 
-  @@feed_length = 6 # This should be a multiple of 6
+  @@feed_length = 1 # This should be a multiple of 6
 
-  before_filter :prepare_feed, :only=>[:latest, :feed]
+  # before_filter :prepare_feed, :only=>[:latest, :feed]
 
   def latest
+    prepare_feed()
+
     respond_to do |format|
       format.html{render action: 'feed'}
       format.js{render action: 'feed'}
@@ -11,10 +13,11 @@ class PostsController < ApplicationController
   end
 
   def tag_feed
-    tags ||= params.has_key?(:tags) ? params[:tags].split(",").each{|x| x.to_sym} : [:all]
+    @tags ||= params.has_key?(:tags) ? params[:tags].split(",").each{|x| x.to_sym} : :all
+    prepare_feed()
 
-    # redirect_to latest_path and return if tags.count == 0
-    redirect_to feed_path(tags.first) and return if tags.count <= 1
+    redirect_to latest_path and return if @tags == :all or @tags.count == 0
+    redirect_to feed_path(@tags.first) and return if @tags.count <= 1
 
     respond_to do |format|
       format.html{render action: 'feed'}
@@ -23,6 +26,9 @@ class PostsController < ApplicationController
   end
 
   def feed
+    @tags ||= params.has_key?(:tag) ? [params[:tag].to_sym] : :all
+    prepare_feed()
+
     respond_to do |format|
       format.html
       format.js
@@ -66,23 +72,23 @@ class PostsController < ApplicationController
   private
 
     def prepare_feed
-      @tag ||= params.has_key?(:tag) ? params[:tag].to_sym : :all
+      @tags ||= :all
       @page ||= params.has_key?(:page) ? params[:page].to_i : 1
       @scroll ||= params.has_key?(:scroll) ? params[:scroll] : :next
 
-      if @tag == :all
-        @posts=Post.active.paginate(:page=>@page, :per_page=>@@feed_length)
-      else
-        @posts=Post.active.tagged_with(@tag).paginate(:page=>@page, :per_page=>@@feed_length)
-      end
-
-      @tags=Post.tag_counts_on(:tags).order('taggings_count DESC')
-
       @selected_tags = []
-      @tags.each{|tag| @selected_tags.push(tag) if tag.name == @tag.to_s}
-      @selected_tags.sort_by{|x| x.name}
+      if @tags == :all
+        @posts=Post.active.paginate(:page=>@page, :per_page=>@@feed_length)
+        @all_tags=Post.tag_counts_on(:tags).order('taggings_count DESC')
+      else
+        @tags = @tags.collect{|x| x.to_sym}
+        @posts=Post.active.tagged_with(@tags).paginate(:page=>@page, :per_page=>@@feed_length)
+        @all_tags=@posts.tag_counts_on(:tags).order('taggings_count DESC')
+        @all_tags.each{|x| @selected_tags.push(x) if @tags.include?(x.name.to_sym) }
 
-      @tags = @tags-@selected_tags
+        @selected_tags.sort_by{|x| x.name}
+        @all_tags = @all_tags-@selected_tags
+      end
 
       @posts.reverse if @scroll == :prev
     end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,8 +3,16 @@ module PostsHelper
   include ActionView::Helpers::NumberHelper
 
   def app_title(stub)
-    wiselinks_title( stub.downcase().to_sym() == :all ? @app_title : "#{stub} | #{@app_title_short}" )
+    wiselinks_title( stub.downcase().to_sym() == :all ? @app_title : "#{stub.to_s.capitalize} | #{@app_title_short}" )
   end 
+
+  def feed_title(tags)
+    if not tags or tags == :all
+      app_title(:all)
+    else
+      tags.count == 1 ? app_title(tags.first) : app_title('Collection')
+    end
+  end
 
   def format_for_counter(num=nil)
     return "" if [nil, 0, "0", ""].include?(num)
@@ -20,6 +28,33 @@ module PostsHelper
   end
 
   def full_post_url(post, **kwargs)
-    post_url(post.id, **kwargs)
+    post_url(post.display_slug, **kwargs)
   end
+
+  def seo_feed_path(tags, **kwargs)
+    tags = seo_feed_params(tags)
+    return latest_path if tags == :all
+    return feed_path(:tag=>tags.first) if tags.length == 1
+    return tag_feed_path(:tags=>tags.join(','))
+  end
+
+  def seo_feed_url(tags, **kwargs)
+    tags = seo_feed_params(tags)
+    return latest_url if tags == :all
+    return feed_url(:tag=>tags.first) if tags.length == 1
+    return tag_feed_url(:tags=>tags.join(','))
+  end
+
+  private
+
+  def seo_feed_params(parms)
+    begin
+      parms = parms.split(",")
+    rescue
+      parms = :all
+    end
+
+    parms
+  end
+
 end
