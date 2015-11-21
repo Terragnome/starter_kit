19c675df3c75cc0beb8ea8a6b56c40881a399be7
Michael Lin <Terragnome@gmail.com>
2015-11-21 07:24:56 -0800

    Add updated_at to Posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021001922) do
+ActiveRecord::Schema.define(version: 20151121152319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20151021001922) do
     t.string   "call_to_action"
     t.string   "url"
     t.string   "slug"
+    t.datetime "updated_at"
   end
 
   add_index "posts", ["active"], name: "index_posts_on_active", using: :btree
