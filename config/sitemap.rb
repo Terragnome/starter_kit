d371836ccb601067cce7c61d11e2c4f8cba02049
Michael Lin <Terragnome@gmail.com>
2015-11-21 07:31:13 -0800

    Update host url

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.#{APP_CONFIG['app_url']}"
+SitemapGenerator::Sitemap.default_host = "http://#{APP_CONFIG['app_url']}"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
