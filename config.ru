9790a7c526dd69ab86c360f29465317b19bd188c
Michael Lin <Terragnome@gmail.com>
2015-12-19 10:55:27 -0800

    Zippy

--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
+require 'rack-zippy'
+use Rack::Zippy::AssetServer
+
 require ::File.expand_path('../config/environment',  __FILE__)
 run Rails.application
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module StarterKit
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
+
     config.exceptions_app = self.routes
 
     ActsAsTaggableOn.force_lowercase = true
